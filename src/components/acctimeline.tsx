57c901e639bf70759636d7f66d9f4abdab7e86fa
vanessa-rodrigues-156 <vanessarodrigues010506@gmail.com>
2024-09-01 22:15:31 +0530

    commit 5

--- src/components/acctimeline.tsx
+++ src/components/acctimeline.tsx
@@ -33,7 +33,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-      className="w-full bg-transparent font-squid md:px-10"
+      className="w-full bg-transparent font-sans md:px-10"
       ref={containerRef}>
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
         <h2 className="text-lg md:text-4xl mb-4 text-white max-w-4xl">
@@ -44,18 +44,17 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           a timeline of my journey.
         </p>
       </div>
-
-      <div
+       <div
         ref={ref}
         className="relative max-w-7xl mx-auto pb-20">
-        {data.map((item, index) => (
-          <div
+        { data.map ((item, index) => (
+        <div
             key={index}
             className="flex justify-start pt-10 md:pt-40 md:gap-10">
-            <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-black flex items-center justify-center">
-                <div className="h-4 w-4 rounded-full bg-[url('/dalagona-candy.png')] border border-neutral-700 p-2" >
-              </div>
+        <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
+            <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-black flex items-center justify-center">
+                <div className="h-4 w-4 rounded-full bg-neutral-800 border border-neutral-700 p-2" >
+                </div>
               <h3 className="hidden md:block text-xl font-squid md:pl-20 md:text-5xl text-neutral-500 ">
                 {item.title}
               </h3>
@@ -67,8 +66,10 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               </h3>
               {item.content}
             </div>
-          </div>
+        </div>
+        </div>
         ))}
+        
         <div
           style={{
             height: height + "px",
@@ -82,9 +83,8 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             className="absolute inset-x-0 top-0  w-[10px] bg-gradient-to-t from-pink-500 via-red-500 to-transparent from-[0%] via-[10%] rounded-full"
           />
         </div>
-
-     </div>
-      </div>
-       </div>
-  );
-};
+    </div>
+    
+    </div>
+    );
+}
