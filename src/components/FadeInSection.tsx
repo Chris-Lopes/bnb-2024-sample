7d59187a91c52cb95ca46f28e4c995ca93b7eefa
ZaneFerns360 <zane.ferns360@gmail.com>
2024-09-26 18:15:05 +0530

    removed fade

--- src/components/FadeInSection.tsx
+++ src/components/FadeInSection.tsx
@@ -1,22 +1,28 @@
-import { ReactNode } from 'react';
-import { motion } from 'framer-motion';
+import { ReactNode } from "react";
 
 interface FadeInSectionProps {
   children: ReactNode;
 }
 
 const FadeInSection: React.FC<FadeInSectionProps> = ({ children }) => {
-  return (
-    <motion.div
-      initial={{ opacity: 0, y: 100 }} // Start from lower down the screen
-      whileInView={{ opacity: 1, y: 0 }} // Move to the middle (original position)
-      transition={{ duration: 1, ease: 'easeInOut' }}
-      viewport={{ once: true }}
-      className="mb-10"
-    >
-      {children}
-    </motion.div>
-  );
+  return <div className="mb-10">{children}</div>;
 };
 
 export default FadeInSection;
+
+// Old code with animation
+// import { motion } from 'framer-motion';
+
+// const FadeInSection: React.FC<FadeInSectionProps> = ({ children }) => {
+//   return (
+//     <motion.div
+//       initial={{ opacity: 0, y: 100 }} // Start from lower down the screen
+//       whileInView={{ opacity: 1, y: 0 }} // Move to the middle (original position)
+//       transition={{ duration: 1, ease: 'easeInOut' }}
+//       viewport={{ once: true }}
+//       className="mb-10"
+//     >
+//       {children}
+//     </motion.div>
+//   );
+// };
